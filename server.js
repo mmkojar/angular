46c173e1ae0c9b03e69f81b1195a6bd891c7a9bb
Mohammed <webdev7909@gmail.com>
2020-09-01 18:08:10 +0530

    new_changes_09

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/blogger'));
+app.use(express.static(__dirname + 'dist/blogger'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/blogger/index.html'));
+    res.sendFile(path.join(__dirname + 'dist/blogger/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
